19aae1cdc28c0663f11d532c49c9d0ec37139428
gihanmora <gihangamage2015@gmail.com>
2026-07-22 14:30:36 +1000

    Bump target API to 36 for Google Play compliance (deadline Aug 31, 2026)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,7 +63,7 @@ val admobAppIdFromConfig = appConfig["admobAppId"] ?: "ca-app-pub-52022532019589
 android {
     // Namespace and applicationId are read from config/country_x.json (single source of truth)
     namespace = packageNameFromConfig
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -82,7 +82,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         
